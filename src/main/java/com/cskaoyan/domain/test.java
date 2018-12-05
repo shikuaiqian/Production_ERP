9e073bb14f920d95f14239619b9bdaddba708e87
shikuaiqian <616906168@qq.com>
2018-12-05 21:36:47 +0800

    第三次提交,增加了目录结构

--- src/main/java/com/cskaoyan/domain/test.java
+++
@@ -1,7 +0,0 @@
-package com.cskaoyan.domain;
-
-/**
- * Created by LZH on 2018/12/5
- */
-public class test {
-}
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -11,4 +11,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:spring/applicationContext.xml")
 public class test {
 
+
 }
