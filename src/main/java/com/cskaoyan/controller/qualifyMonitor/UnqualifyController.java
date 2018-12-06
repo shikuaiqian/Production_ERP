089059ef33841d7180b8af056050d6b1a956ba6c
shikuaiqian <616906168@qq.com>
2018-12-06 12:06:34 +0800

    刘智豪 12/6 12：06

--- src/main/java/com/cskaoyan/controller/qualifyMonitor/UnqualifyController.java
+++ src/main/java/com/cskaoyan/controller/qualifyMonitor/UnqualifyController.java
@@ -1,6 +1,7 @@
 package com.cskaoyan.controller.qualifyMonitor;
 
-//import com.cskaoyan.service.qualifyMonitor.UnqualifyService;
+
+import com.cskaoyan.service.qualifyMonitor.UnqualifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +15,20 @@ import java.util.Map;
 @Controller
 @RequestMapping("/unqualify")
 public class UnqualifyController {
-//    @Autowired
-//    UnqualifyService unqualifyService;
-//
-//    @RequestMapping("find")
-//    public String find(){
-//        return "qualifyMonitor/unqualify_list";
-//    }
-//
-//    @RequestMapping("list")
-//    @ResponseBody
-//    public Map SelectByPage(int page, int rows ){
-//        Map<Object, Object> page1 = unqualifyService.findPage(page, rows);
-//        return page1;
-//
-//    }
+   @Autowired
+   UnqualifyService unqualifyService;
+
+    @RequestMapping("find")
+    public String find(){
+        return "qualifyMonitor/unqualify_list";
+    }
+
+    @RequestMapping("list")
+    @ResponseBody
+    public Map SelectByPage(int page, int rows ){
+        Map<Object, Object> page1 = unqualifyService.findPage(page, rows);
+        return page1;
+
+    }
 
 }
