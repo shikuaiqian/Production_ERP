83a169dcf396d905c76289e8a3b92de88461d382
Erik <451143209@qq.com>
2018-12-06 21:03:58 +0800

    Erik First

---
+++ src/main/java/com/cskaoyan/dao/technologyMonitor/TechnologyRequirementMapper.java
@@ -0,0 +1,31 @@
+package com.cskaoyan.dao.technologyMonitor;
+
+import com.cskaoyan.domain.technologyMonitor.TechnologyRequirement;
+import com.cskaoyan.domain.technologyMonitor.TechnologyRequirementExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TechnologyRequirementMapper {
+    long countByExample(TechnologyRequirementExample example);
+
+    int deleteByExample(TechnologyRequirementExample example);
+
+    int deleteByPrimaryKey(String technologyRequirementId);
+
+    int insert(TechnologyRequirement record);
+
+    int insertSelective(TechnologyRequirement record);
+
+    List<TechnologyRequirement> selectByExample(TechnologyRequirementExample example);
+
+    TechnologyRequirement selectByPrimaryKey(String technologyRequirementId);
+
+    int updateByExampleSelective(@Param("record") TechnologyRequirement record, @Param("example") TechnologyRequirementExample example);
+
+    int updateByExample(@Param("record") TechnologyRequirement record, @Param("example") TechnologyRequirementExample example);
+
+    int updateByPrimaryKeySelective(TechnologyRequirement record);
+
+    int updateByPrimaryKey(TechnologyRequirement record);
+}
