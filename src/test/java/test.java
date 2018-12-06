4e92db32e8c1237f5c6a8026a92bef37bd3ef084
shikuaiqian <616906168@qq.com>
2018-12-06 10:44:41 +0800

    刘智豪 12/6 10:44

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,11 @@
+import com.cskaoyan.service.qualifyMonitor.UnqualifyService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
 
 /**
  * Created by LZH on 2018/12/5
@@ -11,5 +14,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:spring/applicationContext.xml")
 public class test {
 
+    @Autowired
+    UnqualifyService unqualifyService;
+    @Test
+    public void test1(){
+        Map<Object, Object> page = unqualifyService.findPage(1, 10);
+        System.out.println(page);
+    }
 
 }
